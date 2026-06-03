7ba4a7cf65a0d7e161dab3eb000f4b9cc87efe29
mikalastar <miziallaouamalika4@gmail.com>
2026-06-03 19:32:52 +0200

    ulyeeh

--- mes-annonces.php
+++ mes-annonces.php
@@ -1,6 +1,21 @@
 <?php
-session_start();
-$_SESSION['user_id'] = 1;
-require 'connexion.php';
-
+ session_start();
+ $_SESSION['user_id'] = 1;
+ require 'connexion.php';
+ $stmt=$pdo->prepare("SELECT * FROM annonces WHERE user_id=?");
+ $stmt->execute([$_SESSION['user_id']]);
+ $_annonces=$stmt->fetchAll();
 ?>
+<?php require 'header.php'; ?>
+
+<div class="container mt-4">
+    <h2>Mes annonces</h2>
+    <?php foreach($annonces as $annonce): ?>
+        <p> <?=$annonce['titre'] ?> </p>
+        <p> <?=$annonce['prix'] ?>$</p>
+        <p> <?=$annonce['etat'] ?> </p>
+        <p> <?=$annonce['description'] ?> </p>
+        <img src="uploads/<?= $annonce['image'] ?>">
+    <?php endforeach; ?>    
+</div>
+<?php require 'footer.php'; ?>
