89f3145b114483f663f03c3bf478aa8a2ff5cc23
IWWILOL <hassane-messaoudi@hotmail.com>
2026-06-21 00:42:32 +0200

    iwwian again

--- index.php
+++ index.php
@@ -4,11 +4,29 @@ require 'recherche.php';
 
 $result = mysqli_query($conn, "SELECT * FROM annonces ORDER BY id DESC");
 
-$page = $_GET['page'] ?? 1;
+// --- Récupération des filtres ---
+$recherche = $_GET['recherche'] ?? '';
+$etat = $_GET['etat'] ?? '';
+$prixMax = $_GET['prix'] ?? '';
+$page = max(1, (int)($_GET['page'] ?? 1));
 $parPage = 9;
 $offset = ($page - 1) * $parPage;
 
-$sql = "SELECT * FROM annonces ORDER BY id DESC LIMIT $parPage OFFSET $offset";
+// --- Construction de la requête ---
+$sql = "SELECT * FROM annonces WHERE 1=1";
+
+if ($recherche !== '') {
+    $sql .= " AND titre LIKE '%$recherche%'";
+}
+if ($etat !== '') {
+    $sql .= " AND etat = '$etat'";
+}
+if ($prixMax !== '') {
+    $sql .= " AND prix <= '$prixMax'";
+}
+
+$sql .= " ORDER BY id DESC LIMIT $parPage OFFSET $offset";
+
 $result = mysqli_query($conn, $sql);
 
 ?>
