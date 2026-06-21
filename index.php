178affb85ee05dffd0ffa9680032e497f64ab426
IWWILOL <hassane-messaoudi@hotmail.com>
2026-06-21 13:39:59 +0200

    hut

--- index.php
+++ index.php
@@ -28,6 +28,8 @@ require 'recherche.php';
             
             <div class="item">
                 <nav>
+                  <a href="Annonces/mes-annonces.php">voir mes annonces</a>
+                  <a href="Annonces/creer-annonce.php">Publier une annonce</a>
                   <a href="navrecherche.html">Favoris</a>
                   <a href="navrecherche.html">Mes Favoris</a>
                   <a href="Authentification/connexion.php">Connexion</a>
@@ -93,7 +95,7 @@ require 'recherche.php';
 
             </ul>
           </main>
-          
+
         </div>
         <footer>
           test
