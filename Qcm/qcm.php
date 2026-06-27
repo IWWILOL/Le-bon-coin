951e2b13303f4ccfa298f87a23d927e757e9b620
mikalastar <miziallaouamalika4@email.com>
2026-06-27 19:07:10 +0200

    qcmphp

--- Qcm/qcm.php
+++ Qcm/qcm.php
@@ -10,3 +10,35 @@ $result=mysqli_query($conn,$sql);
 $questions=mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <form action="resultat.php" method="post">
+        <?php foreach ($questions as $index => $question): ?>
+            <div>
+                <p><?php echo ($index + 1) . '. ' . htmlspecialchars($question['question']); ?></p>
+                <?php
+                $options = [
+                    $question['reponse1'],
+                    $question['reponse2'],
+                    $question['reponse3'],
+                    $question['reponse4']
+                ];
+                ?>
+                <?php foreach ($options as $option): ?>
+                    <label>
+                        <input type="radio" name="reponse[<?php echo $question['id']; ?>]" value="<?php echo htmlspecialchars($option); ?>" required>
+                        <?php echo htmlspecialchars($option); ?>
+                    </label><br>
+                <?php endforeach; ?>
+            </div>
+            <hr>
+        <?php endforeach; ?>
+        <button type="submit">Soumettre</button>
+    </form>
+</body>
+</html>
