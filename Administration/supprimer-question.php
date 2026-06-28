e9a0c17b9222f77a2f203a15b59b07aa441c4eb2
rahmounizahoua55-svg <rahmounizahoua55@gmail.com>
2026-06-28 17:14:13 +0100

    ughhhh

--- Administration/supprimer-question.php
+++ Administration/supprimer-question.php
@@ -1,5 +1,3 @@
-<?php 
-session_start();
 <?php
 session_start();
 require '../db.php';
@@ -8,15 +6,16 @@ if (!isset($_SESSION['user']) || $_SESSION['user']['role'] !== 'admin') {
     header('Location: ../Authentification/connexion.php');
     exit;
 }
-if($id) {
-  $sql = "DELETE FROM questions WHERE id = ?";
-  $stmt = mysqli_prepare($conn, $sql);
-  mysqli_stmt_bind_param($stmt, "i", $id);
+
+$id = $_GET['id'] ?? null;
+
+if ($id) {
+    $sql = "DELETE FROM questions WHERE id = ?";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, "i", $id);
     mysqli_stmt_execute($stmt);
     mysqli_stmt_close($stmt);
 }
 
 header('Location: admin_questions.php');
 exit;
-
-}
